f373dc8c8a26950beb4be314a8dae7c575bb642b
Giancarlo-lol <59169156+Giancarlo-lol@users.noreply.github.com>
2020-01-10 14:21:03 +0100

    Update caldo-o-freddo.js

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,16 @@
 
   http://www.imparareaprogrammare.it
 */
+var temp = [a = 10, b = -2, c = 31, d = 22, e = 15, f = -6, g = 7];
+var hotD = 0;
+var coldD= 0;
+for (var i=0; i < temp.length; i++){
+  if(temp[i]>hotD){
+    hotD=temp[i];
+  }if(temp[i]<coldD){
+    coldD=temp[i]
+  }
+}
+console.log("la temperatura più alta è : " +hotD+ " mentre quella più bassa è : "+coldD)
+    
+    
