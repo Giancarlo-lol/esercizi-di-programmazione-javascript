8a256a0262b5ca72db02dba5769811cadc81751a
Giancarlo-lol <59169156+Giancarlo-lol@users.noreply.github.com>
2020-01-10 15:19:52 +0100

    Update caldo-o-freddo.js

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -11,14 +11,18 @@
 */
 var temp = [a = 10, b = -2, c = 31, d = 22, e = 15, f = -6, g = 7];
 var hotD = 0;
+var hloc;
 var coldD= 0;
+var cloc;
 for (var i=0; i < temp.length; i++){
   if(temp[i]>hotD){
     hotD=temp[i];
+    hloc=i+1;
   }if(temp[i]<coldD){
-    coldD=temp[i]
+    coldD=temp[i];
+    cloc=i+1;
   }
 }
 console.log("la temperatura più alta è : " +hotD+ " mentre quella più bassa è : "+coldD)
-    
+console.log("la giornata più calda è la " +hloc+ " la giornata più fredda è la " + cloc)    
     
